1bb0674aabe602aa9bc68ebad2d612ef40d7710f
ranquinecode <ranquinecode@gmail.com>
2026-09-01 20:56:17 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,6 +2,6 @@
 set -o errexit
 
 pip install -r requirements.txt
-python manage.py makemigrations
+python manage.py makemigrations loja
 python manage.py migrate
 python manage.py collectstatic --no-input
