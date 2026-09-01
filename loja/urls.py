f2d7b9ecc1597073c1ac546fc89c3e196304a19a
ranquinecode <ranquinecode@gmail.com>
2026-09-01 20:43:29 -0300

    Update urls.py

--- loja/urls.py
+++ loja/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CategoriaViewSet, ProdutoViewSet, PedidoViewSet
+from .views import CategoriaViewSet, ProdutoViewSet, PedidoViewSet, healthcheck
 
 router = DefaultRouter(trailing_slash=True)
 router.register(r'categorias', CategoriaViewSet, basename='categoria')
@@ -9,4 +9,5 @@ router.register(r'pedidos', PedidoViewSet, basename='pedido')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('healthcheck/', healthcheck, name='healthcheck'),
 ]
