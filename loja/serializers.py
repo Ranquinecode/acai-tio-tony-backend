42a1bd88095ba5b07733a31b8d5adf89bb4904e6
ranquinecode <ranquinecode@gmail.com>
2026-09-01 20:40:40 -0300

    Update serializers.py

--- loja/serializers.py
+++ loja/serializers.py
@@ -1,18 +1,37 @@
 from rest_framework import serializers
-from .models import Categoria, ItemAdicional, GrupoOpcao, Produto, Pedido
+from .models import Categoria, ItemAdicional, GrupoOpcao, ItemGrupoOpcao, Produto, Pedido
+
 
 class ItemAdicionalSerializer(serializers.ModelSerializer):
     class Meta:
         model = ItemAdicional
+        fields = ['id', 'nome']
+
+
+class ItemGrupoOpcaoSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='item.id')
+    nome = serializers.ReadOnlyField(source='item.nome')
+    preco = serializers.DecimalField(source='preco_especifico', max_digits=6, decimal_places=2)
+
+    class Meta:
+        model = ItemGrupoOpcao
         fields = ['id', 'nome', 'preco']
 
 
 class GrupoOpcaoSerializer(serializers.ModelSerializer):
-    itens = ItemAdicionalSerializer(many=True, read_only=True)
+    itens = ItemGrupoOpcaoSerializer(source='itens_relacionados', many=True, read_only=True)
 
     class Meta:
         model = GrupoOpcao
-        fields = ['id', 'nome', 'qtd_minima', 'qtd_maxima', 'itens']
+        fields = [
+            'id', 
+            'nome', 
+            'qtd_minima', 
+            'qtd_maxima', 
+            'permitir_exceder', 
+            'preco_item_excedente', 
+            'itens'
+        ]
 
 
 class ProdutoSerializer(serializers.ModelSerializer):
@@ -20,7 +39,19 @@ class ProdutoSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Produto
-        fields = ['id', 'categoria', 'nome', 'preco_base', 'preco_camada_extra', 'grupos_opcoes', 'imagem_url', 'ativo']
+        fields = [
+            'id', 
+            'categoria', 
+            'nome', 
+            'descricao', 
+            'preco_base', 
+            'preco_camada_extra', 
+            'eh_customizavel', 
+            'eh_combo', 
+            'grupos_opcoes', 
+            'imagem_url', 
+            'ativo'
+        ]
 
 
 class CategoriaSerializer(serializers.ModelSerializer):
