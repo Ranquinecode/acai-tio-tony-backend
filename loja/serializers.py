e5fb01519fa5e85cc11f83f24074573376866194
ranquinecode <ranquinecode@gmail.com>
2026-09-01 20:16:06 -0300

    Update serializers.py

--- loja/serializers.py
+++ loja/serializers.py
@@ -4,27 +4,32 @@ from .models import Categoria, ItemAdicional, GrupoOpcao, Produto, Pedido
 class ItemAdicionalSerializer(serializers.ModelSerializer):
     class Meta:
         model = ItemAdicional
-        fields = '__all__'
+        fields = ['id', 'nome', 'preco']
+
 
 class GrupoOpcaoSerializer(serializers.ModelSerializer):
     itens = ItemAdicionalSerializer(many=True, read_only=True)
 
     class Meta:
         model = GrupoOpcao
-        fields = '__all__'
+        fields = ['id', 'nome', 'qtd_minima', 'qtd_maxima', 'itens']
 
-class CategoriaSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Categoria
-        fields = '__all__'
 
 class ProdutoSerializer(serializers.ModelSerializer):
-    categoria = CategoriaSerializer(read_only=True)
     grupos_opcoes = GrupoOpcaoSerializer(many=True, read_only=True)
 
     class Meta:
         model = Produto
-        fields = '__all__'
+        fields = ['id', 'categoria', 'nome', 'preco_base', 'preco_camada_extra', 'grupos_opcoes', 'imagem_url', 'ativo']
+
+
+class CategoriaSerializer(serializers.ModelSerializer):
+    produtos = ProdutoSerializer(many=True, read_only=True)
+
+    class Meta:
+        model = Categoria
+        fields = ['id', 'nome', 'ordem', 'ativo', 'produtos']
+
 
 class PedidoSerializer(serializers.ModelSerializer):
     class Meta:
