33525100835b1cf49d471911354e9e4f1339fe33
ranquinecode <ranquinecode@gmail.com>
2026-09-02 00:58:06 -0300

    Update serializers.py

--- loja/serializers.py
+++ loja/serializers.py
@@ -13,10 +13,17 @@ class ItemGrupoOpcaoSerializer(serializers.ModelSerializer):
     nome = serializers.ReadOnlyField(source='item.nome')
     imagem_url = serializers.ReadOnlyField(source='item.imagem_url')
     preco = serializers.DecimalField(source='preco_especifico', max_digits=6, decimal_places=2)
+    # Recursão para expor os grupos filhos (condicionais) que abrem ao selecionar este item
+    grupos_filhos = serializers.SerializerMethodNested(read_only=True)
 
     class Meta:
         model = ItemGrupoOpcao
-        fields = ['id', 'nome', 'imagem_url', 'preco']
+        fields = ['id', 'nome', 'imagem_url', 'preco', 'grupos_filhos']
+
+    def get_grupos_filhos(self, obj):
+        # Evita importação circular e renderiza os subgrupos
+        serializer = GrupoOpcaoSerializer(obj.grupos_filhos.all(), many=True, context=self.context)
+        return serializer.data
 
 
 class GrupoOpcaoSerializer(serializers.ModelSerializer):
@@ -29,6 +36,7 @@ class GrupoOpcaoSerializer(serializers.ModelSerializer):
             'nome', 
             'qtd_minima', 
             'qtd_maxima', 
+            'permitir_repeticao',
             'permitir_exceder', 
             'preco_item_excedente', 
             'limite_excedente', 
