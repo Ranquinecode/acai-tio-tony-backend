35be4f3c599b5b5a18c320d1e8d006550146738a
ranquinecode <ranquinecode@gmail.com>
2026-09-02 02:15:58 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,7 @@ pip install -r requirements.txt
 echo "🧹 Limpando arquivos estáticos antigos..."
 rm -rf staticfiles
 
-echo "🗄️ Sincronizando o estado das migrações com o Neon.tech..."
-# Marca a migração 0005 como concluída no banco sem tentar re-criar a tabela
-python manage.py migrate loja 0005 --fake
-
-# Garante que todo o resto esteja atualizado
+echo "🗄️ Aplicando migrações no banco Neon.tech..."
 python manage.py migrate --noinput
 
 echo "🎨 Coletando arquivos estáticos..."
