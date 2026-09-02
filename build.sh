9fd21587d8533f065e769e39872c9ef278b71ed8
ranquinecode <ranquinecode@gmail.com>
2026-09-01 23:34:41 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,14 @@
 #!/usr/bin/env bash
+# exit on error
 set -o errexit
 
 pip install -r requirements.txt
+
+# Gera o arquivo de migração se houver alterações no models.py
+python manage.py makemigrations
+
+# Aplica as migrações no banco Neon.tech
 python manage.py migrate
+
+# Coleta os arquivos estáticos sem conflito
 python manage.py collectstatic --no-input
