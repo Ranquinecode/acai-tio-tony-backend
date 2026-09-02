45220f25008e79388a4e4f492bf07320ee35d3e1
ranquinecode <ranquinecode@gmail.com>
2026-09-02 02:09:41 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,11 +8,7 @@ pip install -r requirements.txt
 echo "🧹 Limpando arquivos estáticos antigos..."
 rm -rf staticfiles
 
-echo "📝 Gerando migrações pendentes..."
-python manage.py makemigrations loja --noinput
-
 echo "🗄️ Aplicando migrações no banco Neon.tech..."
-# --fake-initial diz ao Django: se a tabela/coluna já existir no Postgres, não tente recriá-la, apenas marque como aplicada!
 python manage.py migrate --fake-initial --noinput
 
 echo "🎨 Coletando arquivos estáticos..."
