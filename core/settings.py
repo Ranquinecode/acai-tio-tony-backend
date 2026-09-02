44a823e8c3ebedb8adb22568e2a26b31a889244a
ranquinecode <ranquinecode@gmail.com>
2026-09-01 23:38:20 -0300

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -81,9 +81,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'loja', 'static'),
-]
+STATICFILES_DIRS = []
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
